796ed6d3263312a4e880f39682a098358f89f507
unknown <ronaldoa.ojeda@gmail.com>
2025-11-08 21:48:26 -0600

    Test

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
 import swaggerUi from 'swagger-ui-express';
-import cors from 'cors';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - resolveJsonModule enabled
 import apiSpec from './docs/openapi.json';
@@ -10,20 +9,28 @@ import { fxRouter } from './interfaces/http/routes/fx';
 
 const app = express();
 app.use(express.json());
-// CORS primero en la cadena de middlewares
-app.use(cors());
-app.options('*', cors());
 
-// Docs
+/* 🧠 Mueve este bloque aquí (ANTES de cualquier ruta) */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // o 'http://localhost:8080'
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); // <- esto resuelve el preflight
+  }
+  next();
+});
+
+/* Docs */
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
 app.get('/docs-json', (_req, res) => res.json(apiSpec));
 
-// Routes
+/* Routes */
 app.use(openPaymentsRouter);
 app.use(fxRouter);
 
 const { PORT = '3000' } = process.env;
-
 app.listen(Number(PORT), () => {
   console.log(`Open Payments Split (TS) on :${PORT}`);
 });
